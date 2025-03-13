e455d092362fd1fe7114d618ea96014baa6dc4c8
Adam Rumpf <arumpf@floridapoly.edu>
2025-03-13 14:27:28 -0400

    Recording some script plans.

--- scripts/graphics.py
+++ scripts/graphics.py
@@ -5,19 +5,27 @@ result display. This includes both maps to display geospatial distributions as
 well as plots for statistical results.
 """
 
+import csv
+import os.path
+
+import matplotlib.pyplot as plt
 import pygris as pg
+import shapely as shp
 
 #==============================================================================
 # Global Constants
 #==============================================================================
 
 # Shapefile names and paths
-SHP_FL_COUNTIES = "../shapefiles/fl/fl_counties.shp"
-SHP_POLK_TRACTS = "../shapefiles/polk/polk_tracts.shp"
-SHP_POLK_BLOCKS = "../shapefiles/polk/polk_blocks.shp"
+SHP_FL_COUNTIES = os.path.join("..", "shapefiles", "fl", "fl_counties.shp")
+SHP_POLK_TRACTS = os.path.join("..", "shapefiles", "polk", "polk_tracts.shp")
+SHP_POLK_BLOCKS = os.path.join("..", "shapefiles", "polk", "polk_blocks.shp")
+
+# Polk County result file directory
+POLK_RESULTS = os.path.join("..", "results", "polk")
 
 #==============================================================================
-# Data collection
+# Data Collection and Arrangement
 #==============================================================================
 
 def download_shapefiles(state, county, cfile=None, tfile=None, bfile=None):
@@ -49,6 +57,59 @@ def download_shapefiles(state, county, cfile=None, tfile=None, bfile=None):
         blocks = pg.blocks(state=state, county=county)
         blocks.to_file(bfile)
         del blocks
+
+#------------------------------------------------------------------------------
+
+def make_geodata(shapefile, datafile, fields, shapeid="GEOID", dataid="name",
+                 missing=None):
+    """Adds additional data fields to a GeoDataFrame.
+    
+    Positional arguments:
+        shapefile (str) -- File path for the shapefile (tract, block, etc.) to
+            be used as the basis of the GeoDataFrame.
+        datafile (str) -- File path for a data file containing fields to be
+            added to the shapefile's fields.
+        fields (list(str)) -- List of fields in the data file to be added to
+            the GeoDataFrame.
+    
+    Keyword arguments:
+        shapeid (str) -- Field in the shapefile to use as the unique name to
+            distinguish each field. Defaults to "GEOID", which is the full FIPS
+            code for the TIGER/Line shapefiles.
+        dataid (str) -- Field in the data file to use as the unique name to
+            distinguish each field. Should contain exactly the same sets of
+            names as appear in the shapefile's "shapeid" field. Defaults to
+            "name", which is what we've used for our population result files.
+        missing -- Default entry used to fill missing fields. Default None.
+    
+    Returns:
+        (GeoDataFrame) -- A GeoPandas GeoDataFrame object containing the fields
+            from the base file as well as any additional provided fields.
+    
+    To explain, the shapefile and the data file are both expected to have the
+    same number of rows, both containing at least one column full of unique IDs
+    (e.g. FIPS codes) to fully distinguish each row. This function uses the IDs
+    from the shapefile to rearrange the rows from the data file as necessary so
+    that all rows correspond correctly, and then it adds any specified columns
+    to the shapefile, returning the resulting GeoDataFrame.
+    """
+    
+    pass
+
+#==============================================================================
+# Map Generation
+#==============================================================================
+
+### Useful colormap reference: https://matplotlib.org/stable/users/explain/colors/colormaps.html
+
+### To-dos:
+    # Plain tract map with population centroids
+    # Plain tract map with pharmacy locations
+    # Plain tract map with urgent care locations
+    # Tract heatmap with average number of facilities available during [[[time slot]]]
+
+### Implement each of these as a map layer generator, then make a master script
+### that combines several of them.
 
 #==============================================================================
 
